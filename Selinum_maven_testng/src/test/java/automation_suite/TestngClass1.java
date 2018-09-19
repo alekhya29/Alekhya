2fb1e26903d72ad03a5df0300c3a48c61a3606f8
alekhya anumalasetty <alekhya anumalasetty@192.168.0.24>
2018-09-18 23:12:11 -0400

    new1

---
+++ Selinum_maven_testng/src/test/java/automation_suite/TestngClass1.java
@@ -0,0 +1,23 @@
+package automation_suite;
+
+import org.testng.annotations.Test;
+
+public class TestngClass1 {
+  @Test
+  public void C1M1() {
+  
+  
+  System.out.println("C1M1");
+  
+  }
+  
+  
+  
+  @Test
+  public void C1M2()
+  {
+	  
+	  System.out.println("C1M2");
+
+  }
+}
---
+++ Selinum_maven_testng/src/test/java/automation_suite/TestngClass2.java
@@ -0,0 +1,22 @@
+package automation_suite;
+
+import org.testng.annotations.Test;
+
+public class TestngClass2 {
+  @Test
+  public void C2M1() {
+  
+	  System.out.println("C2M1");
+
+  
+  }
+  @Test
+  public void C2M2() {
+	  System.out.println("C2M2");
+
+  }
+  
+
+}
+
+
